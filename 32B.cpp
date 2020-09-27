de1bec1d22d16cc4a70d1fb1eecbcdf740a786e9
09Utkarsh09 <61159067+09Utkarsh09@users.noreply.github.com>
2020-09-27 22:53:10 +0530

    Add files via upload

---
+++ 32B.cpp
@@ -0,0 +1,24 @@
+#include<string.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	string s;
+	int i=0;
+	cin>>s;
+	for(i=0;i<s.length();i++)
+	{
+		if(s[i]=='.')
+		cout<<"0";
+		else if(s[i]=='-' && s[i+1]=='.')
+		{
+		cout<<"1";
+		i++;
+		}
+		else if(s[i]=='-' && s[i+1]=='-')
+		{
+			cout<<"2";
+			i++;
+		}
+	}
+}
